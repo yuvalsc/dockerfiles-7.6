6f7576be20565245f537eccbeefe3c12ff5c1a86
Yuval Schwabe <yuvals@Yuvals-MacBook-Pro-2.local>
2020-04-29 01:17:20 +0200

    Initial Commit

--- clean_build.sh
+++ clean_build.sh
@@ -7,7 +7,9 @@ docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls | tail -n+2 | awk '{if($2 !~ /bridge|none|host/){ print $1 }}')
 docker build --tag yuvalsc/mon-gpp-elastic elasticsearch
 docker build --tag yuvalsc/mon-gpp-kibana kibana
-#docker build --tag yuvalsc/mon-gpp-logstash logstash
+docker build --tag yuvalsc/mon-gpp-logstash logstash
 
-docker run --rm -p 9200:9200 -p 9600:9600 --name es-node1 -d yuvalsc/mon-gpp-elastic
-docker run --rm -p 5601:5601 --name kib-node1 --link es-node1 yuvalsc/mon-gpp-kibana
+docker network create gpp-mon-net
+docker run --rm -p 9200:9200 -p 9600:9600 --name es-node1 --network=gpp-mon-net -d yuvalsc/mon-gpp-elastic
+docker run --rm -p 5601:5601 --name kib-node1 --network=gpp-mon-net -d yuvalsc/mon-gpp-kibana
+docker run --rm -p 5044:5044 --name logstash-node1 --network=gpp-mon-net yuvalsc/mon-gpp-logstash
