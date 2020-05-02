1418f58cadae0c24252be86327d83c4bfc482462
Yuval Schwabe <yuvals@Yuvals-MBP-2.Dlink>
2020-05-02 07:57:46 +0200

    stabilized

--- clean_build.sh
+++ clean_build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+docker stop logstash-node1 stop kib-node1 es-node1
 docker rm -f $(docker ps -a -q)
-
-docker rmi -f $(docker images -a -q)
+docker rmi -f $(docker images -f dangling=true -a -q)
+docker rmi -f yuvalsc/mon-gpp-logstash yuvalsc/mon-gpp-kibana yuvalsc/mon-gpp-elastic
+docker rmi -f $(docker images -f dangling=true -a -q)
 docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls | tail -n+2 | awk '{if($2 !~ /bridge|none|host/){ print $1 }}')
-docker build --tag yuvalsc/mon-gpp-elastic elasticsearch
-docker build --tag yuvalsc/mon-gpp-kibana kibana
-docker build --tag yuvalsc/mon-gpp-logstash logstash
-
 docker network create gpp-mon-net
+
+cd /Users/yuvals/docker/dockerfiles-7.6
+docker build --rm --tag yuvalsc/mon-gpp-elastic elasticsearch
+docker build --rm --tag yuvalsc/mon-gpp-kibana kibana
+docker build --rm --tag yuvalsc/mon-gpp-logstash logstash
+
+docker rmi -f $(docker images -f dangling=true -a -q)
+
+
+
 docker run --rm -p 9200:9200 -p 9600:9600 --name es-node1 --network=gpp-mon-net -d yuvalsc/mon-gpp-elastic
 docker run --rm -p 5601:5601 --name kib-node1 --network=gpp-mon-net -d yuvalsc/mon-gpp-kibana
 docker run --rm -p 5044:5044 --name logstash-node1 --network=gpp-mon-net yuvalsc/mon-gpp-logstash
--- kibana/bin/loadObject.sh
+++ kibana/bin/loadObject.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 set -euo pipefail
-exit 0
+
 kibana_url=http://localhost:5601/api/saved_objects/_import?overwrite=true
 kibana_url_wait=http://localhost:5601/app/kibana#/home
+touch_file=/usr/share/kibana/data/ndjson.loaded
 
-while [[ "$(curl -u "admin:admin" -s -o /dev/null -w '%{http_code}' $kibana_url_wait)" != "200" ]]; do
-    sleep 5
-    echo "waiting.... for $kibana_url_wait"
-done
-# Set the GPP saved objects.
-for filename in ../config/savedObjects/*.ndjson; do
-    [ -e "$filename" ] || continue
-    until echo "Loading $filename: " && curl -sS -u admin:admin -k -X POST "$kibana_url" -H "kbn-xsrf: true" -H "securitytenant: global_tenant" --form file=@$filename && echo
-    do
-    	sleep 2
-    	echo "Retrying... $filename"
+if ! [ -f "$touch_file" ]; then
+	#return_code=""
+	while [[ "$(curl -u "admin:admin" -s -o /dev/null -w '%{http_code}' $kibana_url_wait)" != "200" ]]; do
+		sleep 2
 	done
-
-done
-
-#-H "securitytenant: global_tenant" -H "sg_tenant: Global"
-#curl -u admin:admin -k -X POST "localhost:5601/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@1.ndjson
+	sleep 5
+	# Set the GPP saved objects.
+	for filename in /usr/share/kibana/config/savedObjects/*.ndjson; do
+	    [ -e "$filename" ] || continue
+	    until echo "Loading $filename: " && curl -sS -u admin:admin -k -X POST "$kibana_url" -H "kbn-xsrf: true" --form file=@$filename && echo
+	    do
+	    	sleep 2
+		done
+	done
+	touch $touch_file
+fi
